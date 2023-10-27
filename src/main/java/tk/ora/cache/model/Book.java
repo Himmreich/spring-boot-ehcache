56cfaaf183f03aa800bb478dbabdb6150d8c5b89
Olivier RAZE <olivier.raze@gmail.com>
2023-10-27 09:52:37 +0200

    Upgrade to Java 17

--- src/main/java/tk/ora/cache/model/Book.java
+++ src/main/java/tk/ora/cache/model/Book.java
@@ -1,13 +1,11 @@
 package tk.ora.cache.model;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+
 
 @Data
 @AllArgsConstructor
